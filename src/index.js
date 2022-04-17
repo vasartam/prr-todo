271df40609914aaebabec7e58c613f95d077575b
Василий Артамонов <artamonovvasya10@gmail.com>
2022-04-17 17:13:23 +0300

    Split by components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
 import {createRoot} from 'react-dom/client';
 
-const el = <h1>Hello world!</h1>;
+const Header = () => {
+    return <h1>My Todo List</h1>;
+}
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    );
+}
+
+const SearchPanel = () => {
+    return <input type="search" placeholder="search"/>;
+}
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    )
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(el);
+root.render(<App/>);
