fb7926247e309cbed24d2530118c79585ea01376
Василий Артамонов <artamonovvasya10@gmail.com>
2022-04-17 17:29:09 +0300

    Try JSX features

--- src/index.js
+++ src/index.js
@@ -5,21 +5,40 @@ const Header = () => {
 }
 
 const TodoList = () => {
+    const items = [
+        'Drink Coffee',
+        'Learn React',
+        'Build Awesome App',
+    ];
+
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+            <li>{items[2]}</li>
         </ul>
     );
 }
 
 const SearchPanel = () => {
-    return <input type="search" placeholder="search"/>;
+    const searchPlaceholder = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px',
+    };
+
+    return <input type="search" placeholder={searchPlaceholder} style={searchStyle}/>;
 }
 
 const App = () => {
+    // const isLoggedIn = true;
+    // const loginBox = <span>Log in please</span>;
+    // const welcomeBox = <span>Welcome Back!</span>;
+    // const value = '<script>alert("XSS!");</script>';
+
     return (
         <div>
+            {/*{isLoggedIn ? welcomeBox : loginBox}*/}
+            {/*{value}*/}
             <Header/>
             <SearchPanel/>
             <TodoList/>
