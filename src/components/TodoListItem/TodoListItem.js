b73d0d2475d188f8c6e7a38559028b07c71e144c
Василий Артамонов <artamonovvasya10@gmail.com>
2022-05-06 19:54:03 +0300

    Add `onLabelClick` function with correct `this` binding

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,6 +2,19 @@ import { Component } from 'react';
 import './TodoListItem.scss';
 
 export class TodoListItem extends Component {
+
+  constructor (props, context) {
+    super(props, context);
+
+    this.onLabelClick = () => {
+      console.log(`Done: ${this.props.label}`)
+    }
+  }
+
+  // onLabelClick() {
+  //   console.log(`Done: ${this.props.label}`)
+  // }
+
   render () {
     const { label, important = false } = this.props;
 
@@ -15,7 +28,11 @@ export class TodoListItem extends Component {
         className="todo-list-item"
         style={labelStyle}
       >
-      <span className="todo-list-item__label" style={labelStyle}>
+      <span
+        className="todo-list-item__label"
+        onClick={this.onLabelClick.bind(this)}
+        style={labelStyle}
+      >
         {label}
       </span>
       <div className="todo-list-item__buttons">
@@ -31,7 +48,7 @@ export class TodoListItem extends Component {
                viewBox="0 0 16 16">
             <path
               d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-            <path fill-rule="evenodd"
+            <path fillRule="evenodd"
                   d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
           </svg>
         </button>
