d65d224c464c6ca8cb0a5959918a2af859a3af04
Василий Артамонов <artamonovvasya10@gmail.com>
2022-05-06 20:23:13 +0300

    Convert `App` to a class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,44 @@
+import { Component } from 'react';
 import './App.scss';
 import TodoList from '../TodoList';
 import ItemStatusFilter from '../ItemStatusFilter';
 import SearchPanel from '../SearchPanel';
 import Header from '../Header';
 
-export const App = () => {
-  const todoItems = [
-    {
-      id: 1,
-      label: 'Drink Coffee',
-      important: false,
-    },
-    {
-      id: 2,
-      label: 'Build Awesome App',
-      important: true,
-    },
-    {
-      id: 3,
-      label: 'Have a lunch',
-      important: false,
-    },
-  ];
+export class App extends Component {
+  render () {
+    const todoItems = [
+      {
+        id: 1,
+        label: 'Drink Coffee',
+        important: false,
+      },
+      {
+        id: 2,
+        label: 'Build Awesome App',
+        important: true,
+      },
+      {
+        id: 3,
+        label: 'Have a lunch',
+        important: false,
+      },
+    ];
 
-  return (
-    <div className="app">
-      <Header toDo={1} done={3}/>
+    return (
+      <div className="app">
+        <Header toDo={1} done={3}/>
 
-      <div className="top-panel">
-        <SearchPanel/>
-        <ItemStatusFilter/>
+        <div className="top-panel">
+          <SearchPanel/>
+          <ItemStatusFilter/>
+        </div>
+
+        <TodoList
+          todos={todoItems}
+          onDeleted={(id) => {{console.log('Deleted:', id);}}}
+        />
       </div>
-
-      <TodoList todos={todoItems}/>
-    </div>
-  );
-};
+    );
+  }
+}
