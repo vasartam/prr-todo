ece61ad1015325897184f200b0dbd159ed6a6289
Василий Артамонов <artamonovvasya10@gmail.com>
2022-05-06 19:39:05 +0300

    Convert `ItemStatusFilter` to a class component

--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,20 +1,23 @@
+import { Component } from 'react';
 import './ItemStatusFilter.scss';
 
-export const ItemStatusFilter = () => {
-  return (
-    <div className="item-status-filter btn-group">
-      <button className="btn btn-info"
-              type="button">
-        All
-      </button>
-      <button className="btn btn-outline-secondary"
-              type="button">
-        Active
-      </button>
-      <button className="btn btn-outline-secondary"
-              type="button">
-        Done
-      </button>
-    </div>
-  );
-};
+export class ItemStatusFilter extends Component {
+  render () {
+    return (
+      <div className="item-status-filter btn-group">
+        <button className="btn btn-info"
+                type="button">
+          All
+        </button>
+        <button className="btn btn-outline-secondary"
+                type="button">
+          Active
+        </button>
+        <button className="btn btn-outline-secondary"
+                type="button">
+          Done
+        </button>
+      </div>
+    );
+  }
+}
