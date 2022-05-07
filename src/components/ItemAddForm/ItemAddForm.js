3544cfe725252ab394af2e4545eb5601c350af77
Василий Артамонов <artamonovvasya10@gmail.com>
2022-05-07 19:46:52 +0300

    Finish logic for adding item with custom text

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -2,23 +2,41 @@ import { Component } from 'react';
 import './ItemAddForm.scss';
 
 export class ItemAddForm extends Component {
+  state = {
+    label: '',
+  };
+
   onLabelChange = (e) => {
-    console.log(e.target.value);
-  }
+    this.setState({
+      label: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.label === '') {
+      return;
+    }
+
+    this.props.onItemAdded(this.state.label);
+
+    this.setState({
+      label: '',
+    });
+  };
 
   render () {
     return (
-      <form className="item-add-form">
+      <form className="item-add-form" onSubmit={this.onSubmit}>
         <input
           className="item-add-form__input form-control"
           type="text"
           placeholder="What needs to be done"
           onChange={this.onLabelChange}
+          value={this.state.label}
         />
-        <button
-          className="item-add-form__button btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded('Hello world!')}
-        >
+        <button className="item-add-form__button btn btn-outline-secondary" type="submit">
           Add Item
         </button>
       </form>
