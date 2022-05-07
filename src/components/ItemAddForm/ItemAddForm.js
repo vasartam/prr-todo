ca3777542f31731862943e64c584d0262d7ab443
Василий Артамонов <artamonovvasya10@gmail.com>
2022-05-07 19:40:16 +0300

    Add input field, correct styles

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -2,13 +2,26 @@ import { Component } from 'react';
 import './ItemAddForm.scss';
 
 export class ItemAddForm extends Component {
+  onLabelChange = (e) => {
+    console.log(e.target.value);
+  }
+
   render () {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary" onClick={() => this.props.onItemAdded('Hello world!')}>
+      <form className="item-add-form">
+        <input
+          className="item-add-form__input form-control"
+          type="text"
+          placeholder="What needs to be done"
+          onChange={this.onLabelChange}
+        />
+        <button
+          className="item-add-form__button btn btn-outline-secondary"
+          onClick={() => this.props.onItemAdded('Hello world!')}
+        >
           Add Item
         </button>
-      </div>
+      </form>
     );
   }
 }
