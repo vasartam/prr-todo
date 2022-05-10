9c0d005af822f4635ee186c73938e571cd98523e
Василий Артамонов <artamonovvasya10@gmail.com>
2022-05-10 20:23:04 +0300

    Make search input controllable, extract handler

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -2,13 +2,26 @@ import './SearchPanel.scss';
 import { Component } from 'react';
 
 export class SearchPanel extends Component {
+  state = {
+    search: '',
+  };
+
+  onSearchChange = (e) => {
+    const value = e.target.value;
+
+    this.setState({ search: value });
+
+    this.props.onSearchChanged(value);
+  };
+
   render () {
     return (
       <input
         className="form-control search-input"
         type="search"
         placeholder="Type here to search"
-        onChange={(e) => this.props.onSearchChanged(e.target.value)}
+        value={this.state.search}
+        onChange={this.onSearchChange}
       />
     );
   }
