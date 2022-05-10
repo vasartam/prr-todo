996a04b31982c299a44dfbc50e7fab8e7abc2aef
Василий Артамонов <artamonovvasya10@gmail.com>
2022-05-10 19:10:09 +0300

    Add search

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,24 +2,30 @@ import './TodoList.scss';
 import TodoListItem from '../TodoListItem';
 
 export const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone, search }) => {
-  const todosFiltered = todos;
+  let todosFiltered = todos;
 
   if (search) {
+    const searchSanitized = search.toLowerCase();
 
+    todosFiltered = todosFiltered.filter(todo => todo.label.toLowerCase().includes(searchSanitized));
   }
 
   return (
-    <ul className="todo-list list-group">
-      {todos.map(({ id, ...props }) => (
-        <li className="todo-list__item list-group-item" key={id}>
-          <TodoListItem
-            {...props}
-            onDeleted={() => onDeleted(id)}
-            onToggleImportant={() => onToggleImportant(id)}
-            onToggleDone={() => onToggleDone(id)}
-          />
-        </li>
-      ))}
-    </ul>
+    todosFiltered.length ? (
+      <ul className="todo-list list-group">
+        {todosFiltered.map(({ id, ...props }) => (
+          <li className="todo-list__item list-group-item" key={id}>
+            <TodoListItem
+              {...props}
+              onDeleted={() => onDeleted(id)}
+              onToggleImportant={() => onToggleImportant(id)}
+              onToggleDone={() => onToggleDone(id)}
+            />
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p className="todo-list__message">No items matching your search!</p>
+    )
   );
 };
